76ddb9752dab465968c5508d5a1d879e40a835fe
Nassos A. Michas <nassosmichas@me.com>
2017-02-01 11:19:23 +0200

    Revert "Added missing test in IT suite"
    This reverts commit 1d15e8875ac6a6730a5f844bc3eb4e77c81839fb.

--- devdays103-geocoding-test/src/test/java/com/devdays103/impl/it/GeocodingServiceImplITTest.java
+++ devdays103-geocoding-test/src/test/java/com/devdays103/impl/it/GeocodingServiceImplITTest.java
@@ -1,4 +1,4 @@
-package com.devdays103.impl.mock;
+package com.devdays103.impl.it;
 
 import com.devdays103.api.GeocodingService;
 import com.devdays103.impl.TestUtil;
@@ -11,49 +11,29 @@ import org.ops4j.pax.exam.spi.reactors.PerClass;
 
 import javax.inject.Inject;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
-
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
-public class GeocodingServiceImplMockTest extends MockTestConf {
+public class GeocodingServiceImplITTest extends ITTestConf {
 	@Inject
 	GeocodingService geocodingService;
 
 	@Test
 	public void getGeolocationByAddress() throws Exception {
-		GeocodingService geocodingServiceSpy = spy(geocodingService);
-		doReturn(TestUtil.GEO_ATHENS)
-				.when(geocodingServiceSpy).getGeolocationByAddress(TestUtil.LOC_ATHENS);
-		doReturn(TestUtil.GEO_BELGIUM)
-				.when(geocodingServiceSpy).getGeolocationByAddress(TestUtil.LOC_BRUSSELS);
-		doReturn(TestUtil.GEO_UK)
-			.when(geocodingServiceSpy).getGeolocationByAddress(TestUtil.LOC_UK);
-
 		Assert.assertEquals(TestUtil.GEO_ATHENS,
-				geocodingServiceSpy.getGeolocationByAddress(TestUtil.LOC_ATHENS));
+				geocodingService.getGeolocationByAddress(TestUtil.LOC_ATHENS));
 		Assert.assertEquals(TestUtil.GEO_BELGIUM,
-				geocodingServiceSpy.getGeolocationByAddress(TestUtil.LOC_BRUSSELS));
+				geocodingService.getGeolocationByAddress(TestUtil.LOC_BRUSSELS));
 		Assert.assertEquals(TestUtil.GEO_UK,
-				geocodingServiceSpy.getGeolocationByAddress(TestUtil.LOC_UK));
+				geocodingService.getGeolocationByAddress(TestUtil.LOC_UK));
 	}
 
 	@Test
 	public void getGeolocationByBusiness() throws Exception {
-		GeocodingService geocodingServiceSpy = spy(geocodingService);
-		doReturn(TestUtil.GEO_ATHENS)
-				.when(geocodingServiceSpy).getGeolocationByBusiness("ED-ATH");
-		doReturn(TestUtil.GEO_BELGIUM)
-				.when(geocodingServiceSpy).getGeolocationByBusiness("ED-BEL");
-		doReturn(TestUtil.GEO_UK)
-				.when(geocodingServiceSpy).getGeolocationByBusiness("ED-UK");
-
 		Assert.assertEquals(TestUtil.GEO_ATHENS,
-				geocodingServiceSpy.getGeolocationByBusiness("ED-ATH"));
+				geocodingService.getGeolocationByBusiness("ED-ATH"));
 		Assert.assertEquals(TestUtil.GEO_BELGIUM,
-				geocodingServiceSpy.getGeolocationByBusiness("ED-BEL"));
+				geocodingService.getGeolocationByBusiness("ED-BEL"));
 		Assert.assertEquals(TestUtil.GEO_UK,
-				geocodingServiceSpy.getGeolocationByBusiness("ED-UK"));
+				geocodingService.getGeolocationByBusiness("ED-UK"));
 	}
-
 }
--- devdays103-geocoding-test/src/test/java/com/devdays103/impl/it/ITTestConf.java
+++ devdays103-geocoding-test/src/test/java/com/devdays103/impl/it/ITTestConf.java
@@ -1,4 +1,4 @@
-package com.devdays103.impl.mock;
+package com.devdays103.impl.it;
 
 import com.devdays103.impl.TestUtil;
 import org.ops4j.pax.exam.Configuration;
@@ -9,14 +9,14 @@ import org.ops4j.pax.exam.options.MavenUrlReference;
 
 import java.io.File;
 
-import static com.devdays103.impl.TestUtil.copyMockConf;
+import static com.devdays103.impl.TestUtil.copyITConf;
 import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
 
-public class MockTestConf {
+public abstract class ITTestConf {
 	@Configuration
-	public Option[] config() {
+	public static Option[] config() {
+
 		MavenArtifactUrlReference karafUrl = maven()
 				.groupId("org.apache.karaf")
 				.artifactId("apache-karaf")
@@ -36,15 +36,16 @@ public class MockTestConf {
 						.unpackDirectory(new File("target", "exam"))
 						.useDeployFolder(false),
 				keepRuntimeFolder(),
-				copyMockConf("etc/com.eurodyn.qlack2.util.liquibase.cfg"),
-				copyMockConf("etc/org.ops4j.datasource-devdays103.cfg"),
-				copyMockConf("etc/org.ops4j.pax.url.mvn.cfg"),
+				copyITConf("etc/com.eurodyn.qlack2.util.liquibase.cfg"),
+				copyITConf("etc/org.ops4j.datasource-devdays103.cfg"),
+				copyITConf("etc/org.ops4j.pax.url.mvn.cfg"),
 				logLevel(LogLevelOption.LogLevel.INFO),
 				configureConsole().ignoreLocalConsole(),
 				configureConsole().ignoreRemoteShell(),
-				mavenBundle("org.mockito", "mockito-all", "1.10.19"),
 				features(projectFeaturesRepo, "devdays103-geocoding-deps"),
+				features(projectFeaturesRepo, "pax-jdbc-mysql"),
 				features(projectFeaturesRepo, "devdays103-geocoding")
 		};
 	}
+
 }
